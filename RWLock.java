45873b58cf3d2d0f331082c3dc04e55fa2889766
Karnesh Mehra <karneshmehra@gmail.com>
2013-10-02 05:38:00 -0400

    runnning with cas , till 5 threads

--- RWLock.java
+++ RWLock.java
@@ -9,7 +9,9 @@ class RWLock{
 	}
 	
 	public void lockRead(){
-		while(this.lock.compareAndSet(1,2)==false || this.lock.get()<1)
+		//System.out.println("lock read");
+		while(this.lock.compareAndSet(1,2)==false || this.lock.get()<1){
+			//System.out.println("no read");
 			synchronized (this) {
 				try {
 					this.wait();
@@ -17,24 +19,33 @@ class RWLock{
 					e.printStackTrace();
 				}
 			}
-		this.lock.incrementAndGet();
-		
+		}
+		//System.out.println("got lock");
+		if(this.lock.get()>2)
+			this.lock.incrementAndGet();
 	}
 	
 	public void unlockRead(){
-		if (this.lock.get()>1)
-			this.lock.decrementAndGet();
-		else if(this.lock.get()<-1)
-			this.lock.incrementAndGet();
+		if (this.lock.get()>1){
+			int tmp=this.lock.decrementAndGet();
+			//System.out.println("greater than 1--"+tmp);
+		}
+		else if(this.lock.get()<-1){
+			//System.out.println("less than -1");
+			if(this.lock.incrementAndGet()==-1)
+				this.lock.set(1);
+		}
 		else
-			this.lock.set(1);
+			//System.out.println("elsing");;//this.lock.set(1);
 		synchronized (this) {
 			this.notifyAll();
 		}
 	}
 	
 	public void lockWrite(){
+		//System.out.println("lock write");
 		while(this.lock.compareAndSet(1,0)==false){
+			//System.out.println("no write --"+this.lock.get());
 			if(this.lock.get()>0)
 				this.lock.set(-1*this.lock.get());
 			synchronized (this) {
@@ -45,10 +56,12 @@ class RWLock{
 				}
 			}
 		}
+		//System.out.println("got write");
 	}
 	
 	public void unlockWrite(){
 		this.lock.set(1);
+		//System.out.println("unlock write");
 		synchronized (this) {
 			this.notifyAll();
 		}
