99659f9f37978aff2530e35f38d5602632c88c50
Karnesh Mehra <karneshmehra@gmail.com>
2013-10-02 20:53:44 -0400

    improved further , removed else if  cas

--- RWLock.java
+++ RWLock.java
@@ -10,7 +10,7 @@ class RWLock{
 	
 	public void lockRead(){
 		//System.out.println("lock read");
-		//int tmp=this.lock.get();
+		int tmp=this.lock.get();
 		boolean b=false;
 		while((b=this.lock.compareAndSet(1,2))==false || this.lock.get()<1){
 			synchronized (this) {
@@ -31,36 +31,43 @@ class RWLock{
 	}
 	
 	public void unlockRead(){
-		if (this.lock.get()>1){
-			if(this.lock.decrementAndGet()==1){
-				this.lock.set(1);
+		int tmp,tmp1;
+		if ((tmp=this.lock.get())>1){
+			System.out.println("decing -- "+tmp);
+			
+			boolean ans=this.lock.compareAndSet(tmp,this.lock.get()-1);
+			System.out.println("decedd "+ans+" -- "+this.lock.get());
+			if(this.lock.compareAndSet(1,1)){
 				synchronized (this) {
 					this.notifyAll();
 				}
 			}
 			//System.out.println("gt1 unlock read  --"+this.lock.get());
 		}
-		else if(this.lock.get()<-1){
-			if(this.lock.incrementAndGet()==-1){
-				this.lock.set(1);
+	   if((tmp1=this.lock.get())<-1){
+			this.lock.compareAndSet(tmp1,this.lock.get()+1);
+			if(this.lock.compareAndSet(-1,1)){
 				synchronized (this) {
 					this.notifyAll();
 				}
 			}
 			//System.out.println("lt1 unlock read  --"+this.lock.get());
 		}
-		else
-			System.out.println("elsing");
+		
 	}
 	
 	public void lockWrite(){
 		//System.out.println("lock write");
+		int tmp;
 		while(this.lock.compareAndSet(1,0)==false){
-			System.out.println("no write --"+this.lock.get());
-			if(this.lock.get()>1)
-				this.lock.set(-1*this.lock.get());
+			if((tmp=this.lock.get())>1){
+				System.out.println("minusing -- "+this.lock.get());
+				this.lock.compareAndSet(tmp,-1*this.lock.get());
+				System.out.println("minused == "+this.lock.get());
+			}
 			synchronized (this) {
 				try {
+					System.out.println("no write --"+this.lock.get());
 					if(this.lock.get()!=1)
 						this.wait();
 				} catch (InterruptedException e) {
